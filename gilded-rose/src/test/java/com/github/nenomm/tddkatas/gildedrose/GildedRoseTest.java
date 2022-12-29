be90dbbbf6b4872ebe0bb252e28798d50cba8cf6
nenomm <neno.nenadovic@gmail.com>
2022-12-29 21:33:09 +0100

    gilded rose inital state

---
+++ gilded-rose/src/test/java/com/github/nenomm/tddkatas/gildedrose/GildedRoseTest.java
@@ -0,0 +1,18 @@
+package com.github.nenomm.tddkatas.gildedrose;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class GildedRoseTest {
+
+  @Test
+  void foo() {
+    Item[] items = new Item[]{new Item("foo", 0, 0)};
+    GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    assertEquals("fixme", app.items[0].name);
+  }
+
+}
