076d095b1b21539195eabf26cfbab072d1da88c7
nenomm <neno.nenadovic@gmail.com>
2022-12-30 15:37:01 +0100

    refactor updatequality method

--- gilded-rose/src/main/java/com/github/nenomm/tddkatas/gildedrose/GildedRose.java
+++ gilded-rose/src/main/java/com/github/nenomm/tddkatas/gildedrose/GildedRose.java
@@ -10,6 +10,46 @@ class GildedRose {
 
   public void updateQuality() {
     for (int i = 0; i < items.length; i++) {
+      final Item item = items[i];
+
+      if ("Sulfuras, Hand of Ragnaros".equals(item.name)) {
+        continue;
+      }
+
+      if ("Aged Brie".equals(item.name)) {
+        if (item.sellIn > 0) {
+          item.quality++;
+        } else {
+          item.quality += 2;
+        }
+      } else if ("Backstage passes to a TAFKAL80ETC concert".equals(item.name)) {
+        if (item.sellIn > 10) {
+          item.quality++;
+        } else if (item.sellIn > 5) {
+          item.quality += 2;
+        } else if ((item.sellIn > 0)) {
+          item.quality += 3;
+        } else {
+          item.quality = 0;
+        }
+      } else {
+        // normal item
+        if (item.sellIn > 0) {
+          item.quality--;
+        } else {
+          item.quality -= 2;
+        }
+      }
+
+      item.quality = Math.max(item.quality, 0);
+      item.quality = Math.min(item.quality, 50);
+
+      item.sellIn--;
+    }
+  }
+
+  public void updateQualityOld() {
+    for (int i = 0; i < items.length; i++) {
       if (!items[i].name.equals("Aged Brie")
           && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
         if (items[i].quality > 0) {
