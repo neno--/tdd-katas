8cf04902e3bd9f32e03c3ecfd35abd2ee731173b
nenomm <neno.nenadovic@gmail.com>
2022-12-30 08:10:26 +0100

    add some test to test initial conditions

--- gilded-rose/src/test/java/com/github/nenomm/tddkatas/gildedrose/GildedRoseTest.java
+++ gilded-rose/src/test/java/com/github/nenomm/tddkatas/gildedrose/GildedRoseTest.java
@@ -7,12 +7,178 @@ import org.junit.jupiter.api.Test;
 
 class GildedRoseTest {
 
+  Item item;
+
   @Test
-  void foo() {
-    Item[] items = new Item[]{new Item("foo", 0, 0)};
-    GildedRose app = new GildedRose(items);
+  void sulfurasValuesAlwaysStayTheSame() {
+    final String name = "Sulfuras, Hand of Ragnaros";
+    final int initialSellin = 0;
+    final int initialQuality = 80;
+
+    // given
+    item = new Item(name, initialSellin, initialQuality);
+    GildedRose app = new GildedRose(new Item[]{item});
+
+    // when
     app.updateQuality();
-    assertEquals("fixme", app.items[0].name);
+
+    // then
+    assertEquals(name, app.items[0].name);
+    assertEquals(initialSellin, app.items[0].sellIn);
+    assertEquals(initialQuality, app.items[0].quality);
   }
+
+  @Test
+  void commonItemDecreaseInValueByOne() {
+    final String name = "Elixir of the Mongoose";
+    final int initialSellin = 5;
+    final int initialQuality = 3;
+
+    // given
+    item = new Item(name, initialSellin, initialQuality);
+    GildedRose app = new GildedRose(new Item[]{item});
+
+    // when
+    app.updateQuality();
+
+    // then
+    assertEquals(name, app.items[0].name);
+    assertEquals(4, app.items[0].sellIn);
+    assertEquals(2, app.items[0].quality);
+  }
+
+  @Test
+  void commonItemMinQualityIsZero() {
+    final String name = "Elixir of the Mongoose";
+    final int initialSellin = 5;
+    final int initialQuality = 0;
+
+    // given
+    item = new Item(name, initialSellin, initialQuality);
+    GildedRose app = new GildedRose(new Item[]{item});
+
+    // when
+    app.updateQuality();
+
+    // then
+    assertEquals(name, app.items[0].name);
+    assertEquals(4, app.items[0].sellIn);
+    assertEquals(0, app.items[0].quality);
+  }
+
+  @Test
+  void commonItemQualityDecreasesTwiceAsFastAfterSellByDate() {
+    final String name = "Elixir of the Mongoose";
+    final int initialSellin = 0;
+    final int initialQuality = 5;
+
+    // given
+    item = new Item(name, initialSellin, initialQuality);
+    GildedRose app = new GildedRose(new Item[]{item});
+
+    // when
+    app.updateQuality();
+
+    // then
+    assertEquals(name, app.items[0].name);
+    assertEquals(-1, app.items[0].sellIn);
+    assertEquals(3, app.items[0].quality);
+  }
+
+  @Test
+  void commonItemQualityDecreasesTwiceAsFastAfterSellByDateButCannotBeLessThanZero() {
+    final String name = "Elixir of the Mongoose";
+    final int initialSellin = 0;
+    final int initialQuality = 1;
+
+    // given
+    item = new Item(name, initialSellin, initialQuality);
+    GildedRose app = new GildedRose(new Item[]{item});
+
+    // when
+    app.updateQuality();
+
+    // then
+    assertEquals(name, app.items[0].name);
+    assertEquals(-1, app.items[0].sellIn);
+    assertEquals(0, app.items[0].quality);
+  }
+
+  @Test
+  void agedBrieIncreasesInQuality() {
+    final String name = "Aged Brie";
+    final int initialSellin = 5;
+    final int initialQuality = 1;
+
+    // given
+    item = new Item(name, initialSellin, initialQuality);
+    GildedRose app = new GildedRose(new Item[]{item});
+
+    // when
+    app.updateQuality();
+
+    // then
+    assertEquals(name, app.items[0].name);
+    assertEquals(4, app.items[0].sellIn);
+    assertEquals(2, app.items[0].quality);
+  }
+
+  @Test
+  void agedBrieIncreasesInQualityTwiceAsFastAfterSellByDate() {
+    final String name = "Aged Brie";
+    final int initialSellin = 0;
+    final int initialQuality = 1;
+
+    // given
+    item = new Item(name, initialSellin, initialQuality);
+    GildedRose app = new GildedRose(new Item[]{item});
+
+    // when
+    app.updateQuality();
+
+    // then
+    assertEquals(name, app.items[0].name);
+    assertEquals(-1, app.items[0].sellIn);
+    assertEquals(3, app.items[0].quality);
+  }
+
+  @Test
+  void agedBrieMaxQualityIsFifty() {
+    final String name = "Aged Brie";
+    final int initialSellin = 5;
+    final int initialQuality = 50;
+
+    // given
+    item = new Item(name, initialSellin, initialQuality);
+    GildedRose app = new GildedRose(new Item[]{item});
+
+    // when
+    app.updateQuality();
+
+    // then
+    assertEquals(name, app.items[0].name);
+    assertEquals(4, app.items[0].sellIn);
+    assertEquals(50, app.items[0].quality);
+  }
+
+  @Test
+  void agedBrieMaxQualityIncreasesTwiceAsFastAfterSellByDateButCannotBeMoreThanFifty() {
+    final String name = "Aged Brie";
+    final int initialSellin = 0;
+    final int initialQuality = 49;
+
+    // given
+    item = new Item(name, initialSellin, initialQuality);
+    GildedRose app = new GildedRose(new Item[]{item});
+
+    // when
+    app.updateQuality();
+
+    // then
+    assertEquals(name, app.items[0].name);
+    assertEquals(-1, app.items[0].sellIn);
+    assertEquals(50, app.items[0].quality);
+  }
+
 
 }
