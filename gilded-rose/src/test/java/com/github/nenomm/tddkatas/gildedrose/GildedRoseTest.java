d3861c24fadffdbdb88d4a205139740aacd071b4
nenomm <neno.nenadovic@gmail.com>
2022-12-30 15:09:34 +0100

    complete gilded rose coverage

--- gilded-rose/src/test/java/com/github/nenomm/tddkatas/gildedrose/GildedRoseTest.java
+++ gilded-rose/src/test/java/com/github/nenomm/tddkatas/gildedrose/GildedRoseTest.java
@@ -180,5 +180,81 @@ class GildedRoseTest {
     assertEquals(50, app.items[0].quality);
   }
 
+  @Test
+  void backstagePassesIncreaseInQualityByOne() {
+    final String name = "Backstage passes to a TAFKAL80ETC concert";
+    final int initialSellin = 15;
+    final int initialQuality = 20;
+
+    // given
+    item = new Item(name, initialSellin, initialQuality);
+    GildedRose app = new GildedRose(new Item[]{item});
+
+    // when
+    app.updateQuality();
+
+    // then
+    assertEquals(name, app.items[0].name);
+    assertEquals(14, app.items[0].sellIn);
+    assertEquals(21, app.items[0].quality);
+  }
+
+  @Test
+  void backstagePassesIncreaseInQualityByTwoWhenThereAreTenDaysLeft() {
+    final String name = "Backstage passes to a TAFKAL80ETC concert";
+    final int initialSellin = 10;
+    final int initialQuality = 20;
+
+    // given
+    item = new Item(name, initialSellin, initialQuality);
+    GildedRose app = new GildedRose(new Item[]{item});
+
+    // when
+    app.updateQuality();
+
+    // then
+    assertEquals(name, app.items[0].name);
+    assertEquals(9, app.items[0].sellIn);
+    assertEquals(22, app.items[0].quality);
+  }
+
+  @Test
+  void backstagePassesIncreaseInQualityByThreeWhenThereAreFiveDaysLeft() {
+    final String name = "Backstage passes to a TAFKAL80ETC concert";
+    final int initialSellin = 1;
+    final int initialQuality = 20;
+
+    // given
+    item = new Item(name, initialSellin, initialQuality);
+    GildedRose app = new GildedRose(new Item[]{item});
+
+    // when
+    app.updateQuality();
+
+    // then
+    assertEquals(name, app.items[0].name);
+    assertEquals(0, app.items[0].sellIn);
+    assertEquals(23, app.items[0].quality);
+  }
+
+  @Test
+  void backstagePassesQualityDropsToZeroAfterTheConcert() {
+    final String name = "Backstage passes to a TAFKAL80ETC concert";
+    final int initialSellin = 0;
+    final int initialQuality = 20;
+
+    // given
+    item = new Item(name, initialSellin, initialQuality);
+    GildedRose app = new GildedRose(new Item[]{item});
+
+    // when
+    app.updateQuality();
+
+    // then
+    assertEquals(name, app.items[0].name);
+    assertEquals(-1, app.items[0].sellIn);
+    assertEquals(0, app.items[0].quality);
+  }
+
 
 }
