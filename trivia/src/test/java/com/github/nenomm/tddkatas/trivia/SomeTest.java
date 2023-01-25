052c26646e08de907fcc2c538b00ee734403bdb0
nenomm <neno.nenadovic@gmail.com>
2023-01-25 21:53:14 +0100

    add ugly trivia game kata

---
+++ trivia/src/test/java/com/github/nenomm/tddkatas/trivia/SomeTest.java
@@ -0,0 +1,13 @@
+package com.github.nenomm.tddkatas.trivia;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class SomeTest {
+
+  @Test
+  public void true_is_true() throws Exception {
+    assertTrue(false);
+  }
+}
